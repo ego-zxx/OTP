98c3d29515f79a276c298bc7e247014f144c6751
ego <68725384+ego-zxx@users.noreply.github.com>
2024-02-04 12:30:45 +0545

    Add files via upload

---
+++ app.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+let uniqueOtp; // Declare otp as a global variable
+
+app.post('/sendOTP', (req, res) => {
+    const email = req.body.email;
+    uniqueOtp = req.body.code;
+
+    if (!email) {
+        return res.status(400).send('Email is required');
+    }
+
+    otp = Math.floor(100000 + Math.random() * 900000); // Generate OTP
+
+    const transporter = nodemailer.createTransport({
+        host: 'smtp.zoho.com',
+        port: 465,
+        secure: true,
+        auth: {
+            user: 'rishishahnepal@zohomail.com',
+            pass: 'Rishishah@2'
+        }
+    });
+
+    const mailOptions = {
+        from: 'rishishahnepal@zohomail.com',
+        to: email,
+        subject: 'OTP Verification',
+        text: `Your OTP is: ${uniqueOtp}`
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.error('Error sending email:', error);
+            return res.status(500).send('Failed to send OTP');
+        }
+        console.log('Email sent:', info.response);
+        res.status(200).send('OTP sent successfully');
+    });
+});
+app.use(bodyParser.json());
+
+// Variable to store user data
+let userData = [];
+
+// Route to handle login form submission
+app.post('/login', (req, res) => {
+    const { username } = req.body;
+    const ip = req.ip;
+
+    // Save username and IP address in userData array
+    userData.push({ username, ip });
+
+    // Redirect user to index.html
+    res.redirect('/index.html');
+});
+
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
