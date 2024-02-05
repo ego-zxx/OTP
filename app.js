583431ba557634a79fd74600c31503dc06d427ee
ego <68725384+ego-zxx@users.noreply.github.com>
2024-02-05 17:02:51 +0545

    Add files via upload

---
+++ app.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+let uniqueOtp; // Declare otp as a global variable
+let email;
+function sendOtpEmail(){
+    const transporter = nodemailer.createTransport({
+        host: 'smtp.zoho.com',
+        port: 465,
+        secure: true,
+        auth: {
+            user: 'rishishahnepal@zohomail.com',
+            pass: 'Rishishah@2'
+        }
+    });
+
+    const mailOptions = {
+        from: 'rishishahnepal@zohomail.com',
+        to: email,
+        subject: 'OTP Verification',
+        text: `Your OTP is: ${uniqueOtp}`
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.error('Error sending email:', error);
+            return res.status(500).send('Failed to send OTP');
+        }
+        console.log('Email sent:', info.response);
+       
+    });
+}
+app.post('/sendOTP', (req, res) => {
+     email = req.body.email;
+     uniqueOtp = req.body.code;
+
+    if (!email) {
+        return res.status(400).send('Email is required');
+    }
+   sendOtpEmail();  
+   res.status(200).send('OTP sent successfully');
+});
+
+app.use(bodyParser.json());
+
+
+
+
+app.listen(port, () => {
+    console.log(`Server is running on ${port}`);
+});
